3c4901b883194373b26e9c7414575b42c9c92d2b
MuktiDoshi <doshimukti@gmail.com>
2021-09-24 18:00:38 +0530

    add nightlife

---
+++ client/src/Components/NightLife/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NightLifeCarousal from './NightLifeCarousal';
+
+const NightLife = () => {
+    return (
+        <div>
+            <NightLifeCarousal/>
+            <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+            Nightlife Restaurants in Poonam Sagar Complex, Mira Road
+            </h1>
+        </div>
+    )
+}
+
+export default NightLife;
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import Delivery from './Delivery';
 import Dining from './Dining';
+import NightLife from './NightLife';
 
 const Master = () => {
     const {type} = useParams();
@@ -10,6 +11,7 @@ const Master = () => {
         <div className="my-5">
             {type === "delivery" && <Delivery/>}
             {type === "dining" && <Dining/>}
+            {type === "night" && <NightLife/>}
         </div>
     );
 };
